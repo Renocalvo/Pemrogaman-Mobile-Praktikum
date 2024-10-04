9fce30f5740712584881f969d3e109fcbb4d6894
Saya__IB <indrabayu210903@gmail.com>
2024-10-04 22:02:32 +0700

    version 0.1.1

--- NovelApp/lib/app/modules/baca_page/controllers/baca_controller.dart
+++ NovelApp/lib/app/modules/baca_page/controllers/baca_controller.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
+//import 'package:image_picker/image_picker.dart';
 //import 'dart:io';
 import 'package:flutter/material.dart';
 
